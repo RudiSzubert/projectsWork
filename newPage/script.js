1b64665128deed7e907cd062f935a83053ca8b08
mateusz <mateusz.tadeja@gmail.com>
2017-02-01 18:06:05 +0100

    last today

--- newPage/script.js
+++ newPage/script.js
@@ -6,7 +6,9 @@ $(function(){
 $(document).on('click','.mySlide',function(){
 	console.log((this.id));
 	$("<div class='greyBack'></div>").insertBefore($('.header'));
-	$("<div class='imageFull'><img src='cars/" + this.id +".jpg' /></div>").insertBefore($('.header'));
+	$("<div class='imageFull'><img class='imgFull' src='cars/" + this.id +".jpg' /></div>").insertBefore($('.header'));
+	$('.imgFull').animate({
+		width: '80%'}, 300);
 });
 
 $(document).on('click','.imageFull',function(){
